df256626191c90a1b1c2a657c79271f5a784fe04
byui-programmer-96 <bam21002@byui.edu>
2022-11-11 10:22:20 -0600

    ...

--- developer/Unit05/Constants.cs
+++ developer/Unit05/Constants.cs
@@ -20,7 +20,26 @@ namespace Unit05.Game
 
         public static int FRAME_RATE = 15;
         public static int FONT_SIZE = 15;
-        public static string CAPTION = "Snake";
+        public static string CAPTION = "P1";
+        public static int SNAKE_LENGTH = 8;
+
+        public static Color RED = new Color(255, 0, 0);
+        public static Color WHITE = new Color(255, 255, 255);
+        public static Color YELLOW = new Color(255, 255, 0);
+        public static Color GREEN = new Color(0, 255, 0);
+
+    }
+     public class Constants
+    {
+        public static int COLUMNS = 40;
+        public static int ROWS = 20;
+        public static int CELL_SIZE = 15;
+        public static int MAX_X = 900;
+        public static int MAX_Y = 600;
+
+        public static int FRAME_RATE = 15;
+        public static int FONT_SIZE = 15;
+        public static string CAPTION = "P2";
         public static int SNAKE_LENGTH = 8;
 
         public static Color RED = new Color(255, 0, 0);
