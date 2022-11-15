841a4fb0f94cfb1fbea9bb746f2ffe8a75082590
byui-programmer-96 <bam21002@byui.edu>
2022-11-15 11:21:41 -0600

    ...

--- developer/Unit05/Constants.cs
+++ developer/Unit05/Constants.cs
@@ -1,52 +1,35 @@
-using System;
-using Microsoft.VisualBasic;
-using Unit05.Game.Casting;
-
-namespace Unit05.Game
-{
-    /// <summary>
-    /// <para>A tasty item that snakes like to eat.</para>
-    /// <para>
-    /// The responsibility of Food is to select a random position and points that it's worth.
-    /// </para>
-    /// </summary>
-    public class Constants
-    {
-        public static int COLUMNS = 40;
-        public static int ROWS = 20;
-        public static int CELL_SIZE = 15;
-        public static int MAX_X = 900;
-        public static int MAX_Y = 600;
-
-        public static int FRAME_RATE = 15;
-        public static int FONT_SIZE = 15;
-        public static string CAPTION = "P1";
-        public static int SNAKE_LENGTH = 8;
-
-        public static Color RED = new Color(255, 0, 0);
-        public static Color WHITE = new Color(255, 255, 255);
-        public static Color YELLOW = new Color(255, 255, 0);
-        public static Color GREEN = new Color(0, 255, 0);
-
-    }
-     public class Constants
-    {
-        public static int COLUMNS = 40;
-        public static int ROWS = 20;
-        public static int CELL_SIZE = 15;
-        public static int MAX_X = 900;
-        public static int MAX_Y = 600;
-
-        public static int FRAME_RATE = 15;
-        public static int FONT_SIZE = 15;
-        public static string CAPTION = "P2";
-        public static int SNAKE_LENGTH = 8;
-
-        public static Color RED = new Color(255, 0, 0);
-        public static Color WHITE = new Color(255, 255, 255);
-        public static Color YELLOW = new Color(255, 255, 0);
-        public static Color GREEN = new Color(0, 255, 0);
-
+namespace Namespace {
+    
+    using Color = game.shared.color.Color;
+    
+    public static class Module {
+        
+        public static object COLUMNS = 40;
+        
+        public static object ROWS = 20;
+        
+        public static object CELL_SIZE = 15;
+        
+        public static object MAX_X = 900;
+        
+        public static object MAX_Y = 600;
+        
+        public static object FRAME_RATE = 15;
+        
+        public static object FONT_SIZE = 15;
+        
+        public static object CAPTION = "Cycle";
+        
+        public static object CYCLE_LENGTH = 1;
+        
+        public static object STARTING_POINT = 120;
+        
+        public static object WHITE = Color(255, 255, 255);
+        
+        public static object RED = Color(255, 0, 0);
+        
+        public static object YELLOW = Color(255, 255, 0);
+        
+        public static object GREEN = Color(0, 255, 0);
     }
 }
-
