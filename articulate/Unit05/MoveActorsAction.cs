fe35b9f4a2cc6e05fd5a9ebf747cae9dcdd1f04c
byui-programmer-96 <bam21002@byui.edu>
2022-11-18 16:11:51 -0600

    changes

---
+++ articulate/Unit05/MoveActorsAction.cs
@@ -0,0 +1,13 @@
+using Unit05.Game.Casting;
+using Unit05.Game.Scripting;
+
+namespace Unit05
+{
+    internal class MoveActorsAction : Action
+    {
+        public void Execute(Cast cast, Script script)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
